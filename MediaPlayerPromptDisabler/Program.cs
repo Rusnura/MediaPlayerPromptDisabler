1821ff08b60d8e6ade21a4353176453fa19b2f4e
Tumasoff Ruslan <Rus-nura@yandex.ru>
2021-11-22 21:04:47 +0700

    Added Mutex for single instance application run control

--- MediaPlayerPromptDisabler/Program.cs
+++ MediaPlayerPromptDisabler/Program.cs
@@ -1,17 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace MediaPlayerPromptDisabler
 {
     static class Program
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
+        private static bool isApplicationInstanceCreated = false;
+        private static Mutex instanceMutex = new Mutex(true, "MediaPlayerPromptDisablerApplication", out isApplicationInstanceCreated);
+
         [STAThread]
         static void Main()
         {
+            if (!isApplicationInstanceCreated)
+            {
+                MessageBox.Show("Приложение уже запущено!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
