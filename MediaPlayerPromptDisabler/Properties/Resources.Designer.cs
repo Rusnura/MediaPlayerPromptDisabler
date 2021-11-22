87677f4e6f72226edb328df96178abfacaceedb6
Tumasoff Ruslan <Rus-nura@yandex.ru>
2021-11-23 00:04:11 +0700

    Removed excess resources

--- MediaPlayerPromptDisabler/Properties/Resources.Designer.cs
+++ MediaPlayerPromptDisabler/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace MediaPlayerPromptDisabler.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap mediaPrevious {
-            get {
-                object obj = ResourceManager.GetObject("mediaPrevious", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
