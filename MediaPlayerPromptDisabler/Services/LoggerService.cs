444a64096b31712a4405acc006410034db491f72
Tumasoff Ruslan <Rus-nura@yandex.ru>
2021-11-24 00:16:03 +0700

    LoggerService added

---
+++ MediaPlayerPromptDisabler/Services/LoggerService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MediaPlayerPromptDisabler
+{
+    public class LoggerService
+    {
+        private static LoggerService _instance = null;
+        private ListBox printer;
+
+        private LoggerService() { }
+        public static LoggerService GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new LoggerService();
+            }
+
+            return _instance;
+        }
+
+        public void Log(string message)
+        {
+            if (printer == null)
+                return;
+
+            string currentTime = DateTime.Now.ToString("HH:mm:ss");
+            printer.Items.Insert(0, "[" + currentTime + "] " + message);
+        }
+
+        public void RegisterListBoxPrinter(ListBox printer)
+        {
+            this.printer = printer;
+        }
+    }
+}
